0c98d906b917255372160949ead24e4246260217
Daniel Milewski <Daniel.Milewski86@wp.pl>
2016-09-14 17:01:56 +0200

    10X10 testy

--- test/zadania domowe/Tablica 10x10/script.js
+++ test/zadania domowe/Tablica 10x10/script.js
@@ -66,9 +66,45 @@ $(document).ready(function () {
     
     $('#tabela').append(nowyElement);
 
-    
+
 
     
 //http://kursjs.pl/kurs/super-podstawy/petle.php
 
 })
+var table = document.getElementById('tabela'); //pobieramy tabelkę
+
+var tableHTML = ''; //rozpoczynamy generowanie kodu tabeli
+for (var y=0; y<=10; y++) {
+
+    var tr = '<tr>'; //zaczynam tworzyć nowe tr
+
+    for (var x=0; x<=10; x++) {
+        // var tekst = x*y; //tworzymy tekst do wstawienia do komórki
+
+        if (y==0 || x==0) { //jeżeli jest to pierwsza komórka w pionie lub poziomie
+            var td = '<th>'+'</th>'; //stwórz nowe th
+        } else {
+            var td = '<td>'+'</td>'; //stwórz nowe td
+        }
+
+        tr += td; //wstaw komórkę do rzędu TR
+    }
+    tr += '</tr>'; //zakończ tworzenie rzędu
+
+    tableHTML += tr; //wstaw rząd do generowanego kodu HTML
+}
+
+table.innerHTML = tableHTML; //gotowy wygenerowany kod HTML wstawiamy do tabeli na stronie
+
+
+$(document).ready(function () {
+
+    $('td').eq(35).addClass('player');
+
+
+
+
+//http://kursjs.pl/kurs/super-podstawy/petle.php
+
+});
