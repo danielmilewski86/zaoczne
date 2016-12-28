80b2ba69fda6c8ed868f9b66cb3a27fddd9a01d2
Daniel Milewski <Daniel.Milewski86@wp.pl>
2016-12-28 12:10:33 +0100

    koniec

--- test/JS-sandbox/Powrot do gory strony/script.js
+++ test/JS-sandbox/Powrot do gory strony/script.js
@@ -1,23 +1,45 @@
-(function(){
-    
-    function createButton(){
-        var button = document.createElement("button");
-        
-        button.classList.add("backToTop");
-        button.textContent = "powrot do gory";
-        document.body.appendChild(button);
-        
-        
-        
-        return button;
+(function() {
+
+function createButton() {
+
+    var button = document.createElement("button");
+
+    button.classList.add("backToTop", "hidden");
+    button.textContent = "Powrót do góry";
+    document.body.appendChild(button);
+
+    return button;
+
+}
+
+var button = createButton();
+
+function animateScroll() {
+
+    if(document.body.scrollTop > 0) {
+        window.scrollBy(0, -5);
+        setTimeout(animateScroll, 10);
     }
-    
-    var button = createButton();
-    
-    window.addEventListener("scroll", function(e){
-        
-        if(document.body.scrollTop){
-            
-        }
-    }, false);
+
+}
+
+button.addEventListener("click", function(e) {
+
+    e.stopPropagation();
+
+    animateScroll();
+
+}, false);
+
+window.addEventListener("scroll", function(e) {
+
+    if(document.body.scrollTop >= 100) {
+        button.classList.remove("hidden");
+    } else {
+        button.classList.add("hidden");
+    }
+
+}, false);
+
+
 })();
